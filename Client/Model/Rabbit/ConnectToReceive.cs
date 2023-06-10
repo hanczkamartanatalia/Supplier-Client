fb21afb004d357a841105daaf015d1ed28898980
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-06-10 16:26:39 +0200

    Add connectToReceive body

--- Client/Model/Rabbit/ConnectToReceive.cs
+++ Client/Model/Rabbit/ConnectToReceive.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Client.Model.Rabbit
 {
-    internal class ConnectToReceive
+    internal class ConnectToReceive : QueueRabbitMQ.ConnectionService.Connect
     {
+        private const string queueName = "ClientReceive";
+        private const string hostNameToConnect = "localhost";
+
+        public ConnectToReceive() : base(hostNameToConnect, queueName)
+        {
+            this.HostNameToConnect = hostNameToConnect;
+            this.QueueName = queueName;
+        }
     }
 }
